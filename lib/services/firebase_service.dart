cc94aaaa15f24af268f70bf83541ac20c2d09b00
Logan Luque <57603802+LLuque23@users.noreply.github.com>
2021-11-15 21:54:50 -0500

    - Create home_screen - Create login_screen - Update main.dart to initialize firebase - Update firebase_service to handle login and logout - Update landing_screen to send user to login_screen or home_screen based on current status - Create snackbar component - Create constants.dart to hold app colors and themes

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_app_group_project/components/snackbar.dart';
 
 class FirebaseService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -7,7 +9,65 @@ class FirebaseService {
     return await _auth.signInAnonymously();
   }
 
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  Future<void> userLogout(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Are you sure you want to logout?'),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.red,
+              ),
+              child: const Text('CANCEL'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.green,
+              ),
+              child: const Text('YES'),
+              onPressed: () async {
+                try {
+                  await _auth.signOut();
+                  snackbar(context, 'User successfully logged out.', 5);
+                  Navigator.of(context).pop();
+                } catch (error) {
+                  throw ErrorDescription(error.toString());
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   Stream<User?> userChangesStream() {
     return _auth.userChanges();
+  }
+
+  Future<UserCredential?> loginEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        throw ('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        throw ('Wrong password provided for that user.');
+      }
+    }
   }
 }
